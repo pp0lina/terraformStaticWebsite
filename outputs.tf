85431f3a9e2b333ecd1cb782acf51adc7dec0c60
pp0lina <polinasut@gmail.com>
2024-08-03 12:05:06 +0200

    outputs terraform file

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,5 @@
 # outputs.tf
 output "s3_bucket_website_url" {
   description = "The URL of the S3 static website"
-  value       = aws_s3_bucket.static_website.website_endpoint
+  value       = aws_s3_bucket.website_bucket.website_endpoint
 }
