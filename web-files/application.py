8545a08262fb2863b0886d2ece764063ece900cf
pp0lina <polinasut@gmail.com>
2024-08-09 12:29:07 +0200

    application

--- web-files/application.py
+++ web-files/application.py
@@ -3,9 +3,9 @@ from datetime import datetime
 from flask import Flask, render_template, url_for, request, redirect, make_response
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+application = Flask(__name__)
+application.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+application.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
@@ -20,30 +20,30 @@ class Article(db.Model):
         return '<Article %r>' % self.id
 
 
-@app.route('/')
-@app.route('/home')
+@application.route('/')
+@application.route('/home')
 def index():
     return render_template("index.html")
 
 
-@app.route('/about')
+@application.route('/about')
 def about():
     return render_template("about.html")
 
 
-@app.route('/posts')
+@application.route('/posts')
 def posts():
     articles = Article.query.order_by(Article.date.desc()).all()
     return render_template("posts.html", articles=articles)
 
 
-@app.route('/posts/<int:id>')
+@application.route('/posts/<int:id>')
 def post_detail(id):
     article = Article.query.get(id)
     return render_template("post_detail.html", article=article)
 
 
-@app.route('/posts/<int:id>/del')
+@application.route('/posts/<int:id>/del')
 def post_delete(id):
     article = Article.query.get_or_404(id)
 
@@ -55,7 +55,7 @@ def post_delete(id):
         return "При удалении статьи произошла ошибка."
 
 
-@app.route('/posts/<int:id>/edit', methods=['POST', 'GET'])
+@application.route('/posts/<int:id>/edit', methods=['POST', 'GET'])
 def post_edit(id):
     article = Article.query.get(id)
     if request.method == 'POST':
@@ -72,7 +72,7 @@ def post_edit(id):
         return render_template("post_edit.html", article=article)
 
 
-@app.route('/create-article', methods=['POST', 'GET'])
+@application.route('/create-article', methods=['POST', 'GET'])
 def create_article():
     if request.method == 'POST':
         title = request.form['title']
@@ -91,12 +91,12 @@ def create_article():
         return render_template("create-article.html")
 
 
-@app.route("/login")
+@application.route("/login")
 def login():
     return render_template("login.html")
 
 
-@app.route("/signup")
+@application.route("/signup")
 def signup():
     return render_template("signup.html")
 
@@ -109,7 +109,7 @@ def signup():
     # return res
 
 
-@app.route("/logout")
+@application.route("/logout")
 def logout():
     res = make_response("<p>Вы вышли из аккаунта.</p>")
     res.set_cookie("logged", "", 0)
@@ -117,7 +117,7 @@ def logout():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    application.run(debug=True)
 
 # Flask security
 
