bcf91cd39861c763ceed51e2e2f1923cb105a587
pp0lina <polinasut@gmail.com>
2024-09-09 17:10:38 +0200

    main.tf file

--- main.tf
+++ main.tf
@@ -116,3 +116,21 @@ resource "aws_cloudfront_origin_access_control" "main" {
   signing_protocol                  = "sigv4"
 }
 
+# SSL Certificate for HTTPS
+resource "aws_acm_certificate" "ssl_certificate" {
+  provider                  = aws.use_default_region
+  domain_name               = "*.${var.domain_name_simple}"
+  validation_method         = "DNS"
+  subject_alternative_names = [var.domain_name_simple]
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+# Validating the certificate
+resource "aws_acm_certificate_validation" "cert" {
+  provider                = aws.use_default_region
+  certificate_arn         = aws_acm_certificate.ssl_certificate.arn
+  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
+}
