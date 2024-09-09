2404835a665409ae17147506d35e946b687fc1af
pp0lina <polinasut@gmail.com>
2024-09-09 17:07:11 +0200

    main.tf file

--- main.tf
+++ main.tf
@@ -1,41 +1,75 @@
-# Resources Block
-
 # S3 Bucket for hosting static website files
 resource "aws_s3_bucket" "website_bucket" {
   bucket        = "ekaterina-nutritionist.com"
-  force_destroy = true
+  acl    = "public-read"
+  policy = data.aws_iam_policy_document.allow_public_read.json
+
+    cors_rule {
+    allowed_headers = ["Authorization", "Content-Length"]
+    allowed_methods = ["GET", "POST"]
+    allowed_origins = ["https://${var.domain_name}"]
+    max_age_seconds = 3000
+  }
+
+  website {
+    index_document = "index.html"
+  }
 }
 
 # Block public access to the S3 bucket at the account level
-resource "aws_s3_bucket_public_access_block" "website_bucket" {
-  bucket                  = aws_s3_bucket.website_bucket.id
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
-}
+#resource "aws_s3_bucket_public_access_block" "website_bucket" {
+#  bucket                  = aws_s3_bucket.website_bucket.id
+#  block_public_acls       = false
+#  block_public_policy     = false
+#  ignore_public_acls      = false
+#  restrict_public_buckets = false
+#}
 
 # Upload website files to the S3 bucket
 resource "aws_s3_object" "provision_source_files" {
     bucket  = aws_s3_bucket.website_bucket.id
 
-    for_each = fileset("web-files/", "**/*.*")
+    for_each = fileset("web-files/templates/", "**/*.*")
 
     key    = each.value
-    source = "web-files/${each.value}"
+    source = "web-files/templates/${each.value}"
+}
+
+# IAM policy document for CloudFront access to S3
+data "aws_iam_policy_document" "allow_public_read" {
+  statement {
+    actions   = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.website_bucket.arn}/*"]
+    principals {
+      type        = "Service"
+      identifiers = ["cloudfront.amazonaws.com"]
+    }
+    condition {
+      test     = "StringEquals"
+      variable = "aws:SourceArn"
+      values   = [aws_cloudfront_distribution.cdn_static_site.arn]
+    }
+  }
 }
 
 # Cloudfront for website files distribution
 resource "aws_cloudfront_distribution" "cdn_static_site" {
-  enabled             = true
-  is_ipv6_enabled     = true
-  default_root_object = "index.html"
-
-  origin {
+    origin {
     domain_name              = aws_s3_bucket.website_bucket.bucket_regional_domain_name
     origin_id                = "my-s3-origin" 
     origin_access_control_id = aws_cloudfront_origin_access_control.main.id
+    
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols   = ["TLSv1", "TLSv1.1", "TLSv1.2"]
+    }
   }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = "index.html"
 
   default_cache_behavior {
     allowed_methods  = ["GET", "HEAD", "OPTIONS"]
@@ -48,6 +82,7 @@ resource "aws_cloudfront_distribution" "cdn_static_site" {
 
     forwarded_values {
       query_string = false
+
       cookies {
         forward = "none"
       }
@@ -73,25 +108,6 @@ resource "aws_cloudfront_distribution" "cdn_static_site" {
   ]
 }
 
-# SSL Certificate for HTTPS
-resource "aws_acm_certificate" "ssl_certificate" {
-  provider                  = aws.use_default_region
-  domain_name               = "*.${var.domain_name_simple}"
-  validation_method         = "DNS"
-  subject_alternative_names = [var.domain_name_simple]
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-# Validating the ACM certificate
-resource "aws_acm_certificate_validation" "cert" {
-  provider                = aws.use_default_region
-  certificate_arn         = aws_acm_certificate.cert.arn
-  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
-}
-
 # CloudFront origin access control
 resource "aws_cloudfront_origin_access_control" "main" {
   name                              = "cloudfront oac"
@@ -100,32 +116,3 @@ resource "aws_cloudfront_origin_access_control" "main" {
   signing_protocol                  = "sigv4"
 }
 
-# Output the CloudFront URL
-output "cloudfront_url" {
-  value = aws_cloudfront_distribution.cdn_static_site.domain_name
-}
-
-# IAM policy document for CloudFront access to S3
-data "aws_iam_policy_document" "website_bucket" {
-  statement {
-    principals {
-      type        = "Service"
-      identifiers = ["cloudfront.amazonaws.com"]
-    }
-
-    actions   = ["s3:GetObject"]
-    resources = ["${aws_s3_bucket.website_bucket.arn}/*"]
-
-    condition {
-      test     = "StringEquals"
-      values   = [aws_cloudfront_distribution.cdn_static_site.arn]
-      variable = "aws:SourceArn"
-    }
-  }
-}
-
-# Applying the IAM policy to the S3 bucket
-resource "aws_s3_bucket_policy" "website_bucket_policy" {
-  bucket = aws_s3_bucket.website_bucket.id
-  policy = data.aws_iam_policy_document.website_bucket.json
-}
