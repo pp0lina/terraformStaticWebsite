69a62576d45a552e4540b2ec14e67c3b66514f2a
pp0lina <polinasut@gmail.com>
2024-09-10 17:27:56 +0200

    variables.tf file

--- variables.tf
+++ variables.tf
@@ -1,4 +1,9 @@
 # Variable block
+variable "bucket_name" {
+  type        = string
+  description = "The name of the bucket without the www. prefix. Normally domain_name."
+}
+
 variable "domain_name_simple" {
   description = "The root domain name, e.g., ekaterina-nutritionist.com"
   type        = string
